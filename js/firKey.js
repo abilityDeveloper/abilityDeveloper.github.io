528aad4cf1e81c7a85800abbd9236e0559e551fe
abilityDeveloper <92380927+abilityDeveloper@users.noreply.github.com>
2022-12-14 14:46:33 -0600

    first commit
    first commit

---
+++ js/firKey.js
@@ -0,0 +1,23 @@
+export default class firKey{
+    constructor(){
+        this.nOrganizations = "O"
+        this.nOrgUserMembers = "UM"
+        this.nOrgRecord = "OrganizationRecord"
+        this.nData = "D"
+        this.nArchive = "A"
+        this.nArchiveUserDirctory = "AUD"
+        this.nTasks = "T"
+        this.nLogRecord = "LR"
+        this.nUserDirectory = "UD"
+        this.nLocation = "L"
+        this.nMisc = "Msc"
+        this.nArchiveTasks = "TA"
+        this.nTaskList ="TL"
+        this.nTag = "Tg"
+        this.nUserSettings = "US"
+        this.nFilterPreset = "FP"
+        this.nDeviceDetails = "dd"
+        this.nUser = "U"
+        this.nOrgDetails = "i"
+    }
+}
